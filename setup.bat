7858abc496c581d4ad23bb0b7a3fd914a4d6a831
RealSoerensen <patricklykke@live.dk>
2021-11-22 01:20:28 +0100

    Optimization
    Removal of unnecessary libs and cleaner code

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 python -m pip install opencv-python
-python -m pip install psutil
 python -m pip install numpy
 python -m pip install configparser
 python -m pip install tensorflow-gpu
 python -m pip install keyboard
+python -m pip install pywin32
+python -m pip install mss
 @echo off
