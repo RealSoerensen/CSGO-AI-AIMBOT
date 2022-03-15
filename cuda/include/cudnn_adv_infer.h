7c603841746b97a94cdf883b556e95c4477e4c75
RealSoerensen <patricklykke@live.dk>
2022-03-15 13:01:05 +0100

    Removed CUDA folder and uploaded to mega

--- cuda/include/cudnn_adv_infer.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b2aebf85cc6abd5cd3fcb15c3e0c20c07cf56f624f8cf4c7c8f6301e54d6a613
-size 29683
--- cuda/include/cudnn_ops_train.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f63b8554819c8f5e1fd7b5969a5b9671d2cbfb97614cf8a9cdb77cc59c4101d0
-size 26234
